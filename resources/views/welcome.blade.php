87620f61611409547f9fb14dc68daf0c20dcd72c
Athel-dev <contacto@athel.cl>
2023-04-09 14:08:37 -0400

    Se agrega mensaje en welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,9 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                <div class="h1">
+                    Nuevo texto
+                </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
